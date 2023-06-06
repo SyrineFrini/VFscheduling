7bd65261e49a8ad774933d0b9108daecd496bd0b
Syrine Frini <syrine.frini@rwth-aachen.de>
2023-06-06 12:38:11 +0200

    implemented the sun constraint and different syn categories

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import gurobipy as gp
 from gurobipy import GRB
 
-def solve_crop_optimization(I, R, P, T, theta, W, A, Q):
+def solve_crop_optimization(I, R, P, T, theta, W, A, Q, S, C):
     # Create a new model
     model = gp.Model("crop_optimization")
 
@@ -40,8 +40,19 @@ def solve_crop_optimization(I, R, P, T, theta, W, A, Q):
     # Add constraint 3: Maintenance period for each shelf
     for r in range(1, R+1):
         for p in range(1, P+1):
-            lhs3 = gp.quicksum(X_irpt[3, p, r, t] for t in range(1, T+1))
+            lhs3 = gp.quicksum(X_irpt[4, p, r, t] for t in range(1, T+1))
             model.addConstr(lhs3 == 1)
+
+    # Add Constraint 4: Sun categories
+    for t in range(1,T+1):
+        for p in range(1, P+1):
+            lhs4 = gp.quicksum(X_irpt[i, p, r, t] for i in C[1] for r in S[2]) * gp.quicksum(X_irpt[i, p, r, t] for i in C[1] for r in S[3])
+            lhs5 = gp.quicksum(X_irpt[i, p, r, t] for i in C[2] for r in S[3])
+            lhs6 = gp.quicksum(X_irpt[i, p, r, t] for i in C[3] for r in S[1])
+            model.addConstr(lhs4 == 0)
+            model.addConstr(lhs5 == 0)
+            model.addConstr(lhs6 == 0)
+
 
 
     # Optimize the model
@@ -56,21 +67,24 @@ def solve_crop_optimization(I, R, P, T, theta, W, A, Q):
                     for t in range(1, T+1):
                         solution[f"X_{i}_{p}_{r}_{t}"] = X_irpt[i, p, r, t].x
 
+
     return solution
 
 # Define the data
-I = 3  # Total number of crops // the third one is MAITENANCE
+I = 4  # Total number of crops // the fourth one is MAINTENANCE
 R = 4  # Total number of shelves
 P = 3  # Total number of towers
 T = 5  # Total time horizon (weeks)
 H = 2  # Total number of crop families
-theta = [2, 3, 1]  # Cultivation time of each crop in weeks
+theta = [2, 3, 2, 1]  # Cultivation time of each crop in weeks
 W = [[1, 2, 3, 4], [2, 3, 4, 5], [3, 4, 5, 6]]  # Cultivation area of each shelf in each tower
-A = [[1.5, 2.0, 2.5, 3.0, 3.5], [2.0, 2.5, 3.0, 3.5, 4.0], [0, 0, 0, 0, 0]]  # Price per kilogram of each crop at each week
-Q = [0.8, 0.9, 0]  # Harvested quantity of each crop per cultivation area
+A = [[1.5, 2.0, 2.5, 3.0, 3.5], [2.0, 2.5, 3.0, 3.5, 4.0], [3.0, 4.0, 6.0, 1.0, 4.0], [0, 0, 0, 0, 0]]  # Price per kilogram of each crop at each week
+Q = [0.8, 0.9, 1, 0]  # Harvested quantity of each crop per cultivation area
+C = {1: [1, 2], 2: [3], 3: [4]}
+S = {1: [1], 2: [2], 3: [3]}
 
 # Solve the crop optimization problem
-solution = solve_crop_optimization(I, R, P, T, theta, W, A, Q)
+solution = solve_crop_optimization(I, R, P, T, theta, W, A, Q, C, S)
 
 # Print the optimal solution
 for var, val in solution.items():

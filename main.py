8a84eb396235beb91617350935b58306f2ad5cdf
Syrine Frini <syrine.frini@rwth-aachen.de>
2023-06-09 00:30:12 +0200

    changes to the modelling function

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ from gurobipy import GRB
 import matplotlib.pyplot as plt
 import random
 
+import random
+import matplotlib.pyplot as plt
+
 def generate_gantt_chart(solution, I, R, P, T, theta):
     # Create a list of crops
     crops = ["Crop {}".format(i) for i in range(1, I+1)]
@@ -10,12 +13,12 @@ def generate_gantt_chart(solution, I, R, P, T, theta):
     # Create a dictionary to store the job schedule for each tower
     tower_schedules = {}
 
+    # Create a color map to assign colors to crops
+    color_map = {crops[i]: plt.cm.get_cmap('tab20')(i / I) for i in range(I)}
+
     for p in range(1, P+1):
         # Initialize the schedule for the current tower
         tower_schedule = [[] for _ in range(R)]
-
-        # Assign colors to each crop
-        crop_colors = {crops[i]: random.choice(['#FF0000', '#00FF00', '#0000FF', '#FFFF00', '#00FFFF', '#FF00FF']) for i in range(I)}
 
         # Iterate over the shelves and time periods
         for r in range(1, R+1):
@@ -24,8 +27,8 @@ def generate_gantt_chart(solution, I, R, P, T, theta):
                 for i in range(1, I+1):
                     if solution.get("X_{}_{}_{}_{}".format(i, p, r, t), 0) == 1:
                         crop_length = theta[i]
-                        crop_start = ((t - 1) % T) + 1  # Adjust start time if rotation is needed
-                        tower_schedule[r-1].append((crop_start, crop_length, crops[i-1], crop_colors[crops[i-1]]))
+                        crop_start = ((t) % T)  # Adjust start time if rotation is needed
+                        tower_schedule[r-1].append((crop_start, crop_length, crops[i-1]))
 
         # Store the schedule for the current tower
         tower_schedules[p] = tower_schedule
@@ -36,7 +39,8 @@ def generate_gantt_chart(solution, I, R, P, T, theta):
 
         for r in range(R):
             for job in tower_schedule[r]:
-                ax.barh(r, job[1], align='center', height=0.5, left=job[0], color=job[3])
+                color = color_map[job[2]]
+                ax.barh(r, job[1], align='center', height=0.5, left=job[0], color=color)
                 ax.text(job[0] + job[1] / 2, r + 0.25, job[2], ha='center', va='center')
 
             ax.set_yticks(range(R))
@@ -45,6 +49,7 @@ def generate_gantt_chart(solution, I, R, P, T, theta):
             ax.set_title("Tower {}".format(p))
 
         plt.show()
+
 
 def get_adjacent_tuples(tuples):
     adjacent_tuples = []
@@ -124,9 +129,8 @@ def solve_crop_optimization(I, R, P, T, theta, W, A, Q, S, C, Z, G, F):
                 lhs7 = gp.quicksum(
                             X_irpt[i, p1, r1, t - z + T] + X_irpt[i, p2, r2, t - z + T]  if (t - z) <= 0 else X_irpt[i, p1, r1, t - z ] + X_irpt[i, p2, r2, t - z]
                             for i in F[h]
-                            for z in range(0, theta[i]+1))
+                            for z in range(0, theta[i]))
                 model.addConstr(lhs7 <= 1)
-
 
 
     # Optimize the model

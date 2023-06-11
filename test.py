64cb310106457328852089df57e541516db97bdb
Syrine Frini <syrine.frini@rwth-aachen.de>
2023-06-11 10:43:56 +0200

    changes to the modelling function

--- test.py
+++ test.py
@@ -212,6 +212,34 @@ for i in range(1, I+1):
     Z[i] = crops_df["Average Height (cm)"][i]
 print(Z)
 
+G = {}
+for p in range(1, P+1):
+    for z in range(1, R+1):
+        G[p, z] = shelves_df["height"][z]
+print(G)
+
+'''W = {1: [1, 2, 3, 4], 2: [2, 3, 4, 5], 3: [3, 4, 5, 6]}  # Cultivation area of each shelf in each tower
+A = {1: [1.5, 2.0, 2.5, 3.0, 3.5], 2: [2.0, 2.5, 3.0, 3.5, 4.0], 3: [3.0, 4.0, 6.0, 1.0, 4.0],
+     4: [0, 0, 0, 0, 0]}  # Price per kilogram of each crop at each week
+Q = {1: 0.8, 2: 0.9, 3: 1, 4: 0}  # Harvested quantity of each crop per cultivation area
+C = {1: [1, 2], 2: [3], 3: [4]}  # Sun categories of the selves
+S = {1: [1], 2: [2], 3: [3]}  # sun categories of the plants
+F = {1: [1, 2], 2: [3]}  # Families of the plant
+Z = {1: 5, 2: 7, 3: 4, 4: 0}  # Average height of each crop
+G = {(1, 1): 10, (1, 2): 5, (1, 3): 8, (1, 4): 12, (2, 1): 7, (2, 2): 9, (2, 3): 6, (2, 4): 11, (3, 1): 9, (3, 2): 6,
+     (3, 3): 10, (3, 4): 8}'''
+
+# Generate the set of tuples (p1, r1, p2, r2)
+cartesian_product = [(p1, r1, p2, r2) for p1 in range(1, P + 1) for r1 in range(1, R + 1) for p2 in range(1, P + 1) for
+                     r2 in range(1, R + 1)]
+
+# Get the adjacent tuples
+adjacent_tuples = get_adjacent_tuples(cartesian_product)
+print(adjacent_tuples)
+
+# Solve the crop optimization problem
+solution = solve_crop_optimization(I, R, P, T, H, theta, W, A, Q, C, S, Z, G, F)
+
 # Print the optimal solution
 for var, val in solution.items():
     print(f"{var} = {val}")
